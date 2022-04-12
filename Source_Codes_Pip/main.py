7217c1ee5f260acd22b4294a972361b41d567df1
Ernie Lozano <ejplozano@gmail.com>
2022-04-12 07:43:10 -0700

    added python 3.10 i think

--- Source_Codes_Pip/main.py
+++ Source_Codes_Pip/main.py
@@ -1,2 +1,15 @@
+# Overhead Main codes that the Raspberry Pi will run
 import ADC_Senior_Project
+from machine import Pin
+from machine import ADC
+
+
+PA_main = {
+    "ADC_Ch_0"      : 26,                               # 1st channel for onboard ADC
+    "ADC_Ch_1"      : 27                                # 2nd channel for onboard ADC
+}
+
+# Initialize ADC Pins
+Ph_adc_pin = ADC(Pin(PA_main["ADC_Ch_0"]))              # Init Phase ADC Pin
+Mag_adc_pin = ADC(Pin(PA_main["ADC_Ch_1"]))             # Init Mag ADC Pin
 

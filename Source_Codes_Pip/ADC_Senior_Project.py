7d2e875301c1a2e16946061418da14ef0af09ecb
PipPipDoodlyDoo <103150175+PipPipDoodlyDoo@users.noreply.github.com>
2022-04-14 22:35:36 -0700

    Adding more to main

--- Source_Codes_Pip/ADC_Senior_Project.py
+++ Source_Codes_Pip/ADC_Senior_Project.py
@@ -1,3 +1,6 @@
-ADC_SP = {
+from machine import ADC
 
+ADC_SP = {
+    "BIT_RES"   : 65535,                # Denominator for Dig-2-Analog conversion
+    "REF_VOLT"  : 3.3                   # Reference Voltage
 }
--- Source_Codes_Pip/main.py
+++ Source_Codes_Pip/main.py
@@ -8,6 +8,8 @@ PA_main = {
     "ADC_Ch_0"      : 26,                               # 1st channel for onboard ADC
     "ADC_Ch_1"      : 27                                # 2nd channel for onboard ADC
 }
+# initialize the adc measurements as an array in a global level
+ADC_mes = [0, 0]    # [0]: Phase, [1]: Magnitude
 
 # Initialize ADC Pins
 Ph_adc_pin = ADC(Pin(PA_main["ADC_Ch_0"]))              # Init Phase ADC Pin
@@ -15,8 +17,10 @@ Mag_adc_pin = ADC(Pin(PA_main["ADC_Ch_1"]))             # Init Mag ADC Pin
 
 # Might want to work on a calibration stage
 
+
 # This will be the forever loop
 while True:
+
     # Run the ADC Measurements
     # Calculate the Phase Array
     # Display direction to user

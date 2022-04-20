37a4560bc319e2595f6c0344c362a93ec4f440b6
PipPipDoodlyDoo <103150175+PipPipDoodlyDoo@users.noreply.github.com>
2022-04-19 22:24:44 -0700

    created an ADC test file that should read the values. It is also a good practice to look into indexing and for loops through python

--- Senior_Project.py
+++ Senior_Project.py
@@ -2,13 +2,12 @@ import math
 
 # Dictionary for the source file.
 ADC_SP = {
-    "BIT_RES": 65535,  # Denominator for Dig-2-Analog conversion
-    "REF_VOLT": 3.3,  # Reference Voltage
-    "FREQ": 165500000,  # Frequency to 165.5 MHz
-    "DIST": 3,  # distance between antenna in meters
-
+    "BIT_RES"   : 65535,  # Denominator for Dig-2-Analog conversion
+    "REF_VOLT"  : 3.3,  # Reference Voltage
+    "FREQ"      : 165500000,  # Frequency to 165.5 MHz
+    "DIST"      : 3,  # distance between antenna in meters
+    "BETA"      :
 }
-
 
 # This function will convert the digital value to analog
 def dig_2_ana(dig_value):
@@ -16,14 +15,13 @@ def dig_2_ana(dig_value):
     return analog_value
 
 # Convert the voltage received to phase difference
-def volt_2_ph(voltage):
-    phase = voltage
-    return phase
-
-# Convert the voltage to magnitude difference.
-def volt_2_mag(voltage):
-    magnitude = voltage
-    return magnitude
+def volt_2_ph(voltage, half):
+    if half == 1:
+        phase = -0.0105 * voltage + 1.8685
+        return phase
+    if half == 0:
+        phase =  0.0106 * voltage + 1.878
+        return phase
 
 # Calculate angle to the beacon signal
 def Phase_array_calc(phase):
--- main.py
+++ main.py
@@ -25,11 +25,11 @@ in_pin = Pin(0, Pin.IN, Pin.PULL_DOWN)  # Set the initial value to zero
 
 # Initialize ADC Pins
 Ph_adc_pin = ADC(Pin(PA_main["ADC_Ch_0"]))              # Init Phase ADC Pin
-Mag_adc_pin = ADC(Pin(PA_main["ADC_Ch_1"]))             # Init Mag ADC Pin
+# Mag_adc_pin = ADC(Pin(PA_main["ADC_Ch_1"]))             # Init Mag ADC Pin
 
 # Set up the variables that will be used
 ph_mes = []                                             # This will be useful for the calibration process
-mag_mes = []                                            # Don't know if we'll need this later
+# mag_mes = []                                            # Don't know if we'll need this later
 cal_prog = 0                                            # use to jump out of calibration process
 
 # Indicate to the user that the Initialization is done
@@ -41,15 +41,15 @@ print("Initialization is complete!")
 while True:
     # Run the ADC Measurements
     Ph_volt  = Ph_adc_pin.read_u16()
-    Mag_volt = Mag_adc_pin.read_u16()
+#    Mag_volt = Mag_adc_pin.read_u16()
 
 
     # Convert both digital value to analog
     Ph_volt  = Senior_Project.dig_2_ana(Ph_volt)
-    Mag_volt = Senior_Project.dig_2_ana(Mag_volt)
+#    Mag_volt = Senior_Project.dig_2_ana(Mag_volt)
 
     # Use the conversion formula for voltage -> Phase & Mag
-
+    Ph_volt = Senior_Project.volt_2_ph(Ph_volt, 1)          # This should make Ph_volt in degrees
 
     # Calculate the Phase Array
     direction = Senior_Project.Phase_array_calc(Ph_volt)

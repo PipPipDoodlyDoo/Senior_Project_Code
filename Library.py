ec382cbc221a054f4430ab3342b70f06a0667b0a
Ernie Lozano <ejplozano@gmail.com>
2022-05-12 14:19:36 -0700

    Added a new version of the code and reformat legacy code. This will indicate what version of code is within. -[] The new code should handle the highest output of the phase and lowest output. This would cause ambiguity whether we are shifting more one direction becuase the output is parabolic. -[] Add Variables that will respresent the lowest and highest.
    Update: 5-1-2021
     -added "Multiple_Interrupt_Test" to "Test_Codes" directory. This will be incorporated to the final code. Need to test if within interrupt that will exit the interrupt protocol when button is released
     -Finished the slope_direction interrupt. LED on will indicate that we are using the rising slope and off will show that falling slope. Initially on the rising so the led is on after calibration. USE THE LED FOR DEBUGGING
     - Adjusted the "Multiple_Interrupt_Test" code and the main code for version 3 to take account of DEBOUNCING
     -added a function to change the max and min of phase output
     Update: 5-2-2022
     -24 days til Senior Project Expo
    
     Update: 5-10-2022
     next thing to do is configure the main on how to handle the calculation of the offset.
     and then working with the index

---
+++ Library.py
@@ -0,0 +1,40 @@
+# DICTIONARY "LIBRARY OF VARIABLES THAT WILL BE USED"
+MAIN_LIB = {
+    "ADC_Ch_0"          : 26,                           # Phase Channel
+    "ADC_Ch_1"          : 27,                           # Magnitude Channel
+    "ADC_Ch_2"          : 28,                           # Extra ADC Channel
+    "cal_in_pin"        : 18,                           # Calibration Input Pin
+    "cal_out_pin"       : 20,                           # Calibration Output Pin
+    "CONFIRM_OUT_PIN"   : 14,                           # Confirmation Output Pin for Pre-Measurement Protocol
+    "CONFIRM_IN_PIN"    : 15,                           # Confirmation Input Pin for Pre-Measurement Protocol
+    "DEBUG_OUT_PIN"     : 10,
+    "DEBUG_IN_PIN"      : 11,
+    "PAUSE_TIME"        : 10,
+    # Reference Pin
+    "RISING_SLOPE"      : 1,
+    "FALLING SLOPE"     : 0,
+    "UPPER_VOLT_THRES"  : 1.65,                         # Threshold Voltages for Phase Output of AD8302
+    "LOWER_VOLT_THRES"  : 0.15,
+    "DEFAULT_PH_SHIFT"  : 0,                            # Default Phase Shift used for forcing overlapping elements to 0
+    "DEFAULT_PH_SH_UP"  : 180,                          #
+    "CENTER_REGION"     : 0,                            # No overlap
+    "UPPER_REGION"      : 1,                            # Overlap on the phase lead
+    "LOWER_REGION"      : -1,                           # Overlap on Phase Lag sides
+    "DEBOUNCE_SL"       : 1,                            # Debounce sleeping time
+    "MAG_SAMPLES"       : 15,                            # Amount of samples taken'
+    "MAG_BUFFER"        : 0.1,                           # Buffer Voltage to check for the overlap. Use with UPPER AND LOWER THRESHOLD VOLTAGES
+    "REF_VOLT"          : 3.3,                      # Reference Voltage for ADC
+    "BIT_RES"           : 65535,                    # A bit of Resolution for ADC
+    "DIST"              : 1.65,                     # distance between antennas [meters] (76 cm)
+    "BETA"              : 3.466,                    # Beta for 165.5 MHz
+    "LAMBDA"            : 1.812,                    # wavelength for 165.5 MHz
+    "NUM_ELEM"          : 2,                        # Number of Elements
+    "3 o'clock"         : 0,                        # direction headings
+    "2 o'clock"         : 1,
+    "1 o'clock"         : 2,
+    "12 o'clock"        : 3,
+    "11 o'clock"        : 4,
+    "10 o'clock"        : 5,
+    "9 o'clock"         : 6,
+    "ERROR"             : 7
+}
--- Senior_Project.py
+++ Senior_Project.py
@@ -1,44 +1,21 @@
 # Source Code for ALT_CODE
 import math
-
+import Library
 # DICTIONARY FOR SOURCE FILE
-SP_LIB = {
-    "REF_VOLT"          : 3.3,                      # Reference Voltage for ADC
-    "BIT_RES"           : 65535,                    # A bit of Resolution for ADC
-    "DIST"              : 1.65,                     # distance between antennas [meters] (76 cm)
-    "BETA"              : 3.466,                    # Beta for 165.5 MHz
-    "LAMBDA"            : 1.812,                    # wavelength for 165.5 MHz
-    "NUM_ELEM"          : 2,                        # Number of Elements
-    "3 o'clock"         : 0,                        # direction headings
-    "2 o'clock"         : 1,
-    "1 o'clock"         : 2,
-    "12 o'clock"        : 3,
-    "11 o'clock"        : 4,
-    "10 o'clock"        : 5,
-    "9 o'clock"         : 6,
-    "ERROR"             : 7,
-    "RISING_SLOPE"      : 1,
-    "FALLING_SLOPE"     : 0,
-    "DEFAULT_PH_SHIFT"  : 0,                        # Default Phase Shift used for forcing overlapping elements to 0
-    "DEFAULT_PH_SH_UP"  : 180,                      # From the calculations and slope manipulation this is what came out
-    "CENTER_REGION"     : 0,                        # No overlap
-    "UPPER_REGION"      : 1,                        # Overlap on the phase lead
-    "LOWER_REGION"      : -1                        # Overlap on Phase Lag sides
-}
 
 # DIGITAL TO ANALOG CONVERTOR
 def dig_2_ana(dig_value):
-    analog_value = SP_LIB["REF_VOLT"] / SP_LIB["BIT_RES"] * dig_value
+    analog_value = Library.MAIN_LIB["REF_VOLT"] / Library.MAIN_LIB["BIT_RES"] * dig_value
     print("\nAnalog Voltage: ", '{:2f}'.format(analog_value))         # print out the analog voltage
     return analog_value
 
 # ANALOG TO PHASE OFFSET CONVERTOR
 def volt_2_ph(voltage):
-    # if half == SP_LIB["FALLING_SLOPE"]:
+    # if half == Library.MAIN_LIB["FALLING_SLOPE"]:
     #     phase = abs(-94.786 * voltage + 177.15)
     #     print('Phase Offset = ', phase)
     #     return phase
-    # elif half == SP_LIB["RISING_SLOPE"]:
+    # elif half == Library.MAIN_LIB["RISING_SLOPE"]:
     #     #        phase = abs(94.476 * voltage - 177.44)                         # Original
     #     phase = abs(phase)
 
@@ -56,16 +33,16 @@ def volt_2_ph(voltage):
     return phase
 
 def phase_offset_calculation(index, current_phase, calibrated_phase):
-    if index == SP_LIB["CENTER_REGION"]:
+    if index == Library.MAIN_LIB["CENTER_REGION"]:
         phase_offset = abs(calibrated_phase - current_phase)
         return phase_offset
 
-    elif index == SP_LIB["UPPER_REGION"]:
+    elif index == Library.MAIN_LIB["UPPER_REGION"]:
         # SINCE THIS IS A PEAK, THE CONTRIBUTION IS OFF BY 180 DEGREES THAT IS WHY WE HAVE A 180 - CURRENT PHASE MEASUREMENTS
-        phase_offset = (SP_LIB["DEFAULT_PH_SH_UP"] - calibrated_phase) + (180 - current_phase)
+        phase_offset = (Library.MAIN_LIB["DEFAULT_PH_SH_UP"] - calibrated_phase) + (180 - current_phase)
         return phase_offset
 
-    elif index == SP_LIB["LOWER_REGION"]:
+    elif index == Library.MAIN_LIB["LOWER_REGION"]:
         # THIS IS THE TROPH OUTPUT OF AD83O2 THEREFORE THE CONTRIBUTION OF PHASE OFFSET IS
         # A SIMPLE ADDITION
         phase_offset = (calibrated_phase + current_phase)
@@ -75,7 +52,7 @@ def phase_offset_calculation(index, current_phase, calibrated_phase):
 # PHASE ARRAY CALCULATION: (Relative to drone's flying direction)
 def Phase_array_calc(phase):
     phase = math.radians(phase)
-    theta = (phase * SP_LIB["NUM_ELEM"] * SP_LIB["LAMBDA"])/(2 * math.pi * SP_LIB["DIST"])  # General Equation for Phase Array
+    theta = (phase * Library.MAIN_LIB["NUM_ELEM"] * Library.MAIN_LIB["LAMBDA"])/(2 * math.pi * Library.MAIN_LIB["DIST"])  # General Equation for Phase Array
     theta = math.asin(theta)                                                                # Inverse Sine in radians
     theta = theta * 180 / math.pi                                                           # Convert to Degrees
     return theta
@@ -83,39 +60,39 @@ def Phase_array_calc(phase):
 # USE THE INFORMATION TO SHOW USER WHERE BACON (lol) SIGNAL IS
 def dir_to_heading(degree, direction):
     if (degree <= 15) and (degree >= 0):
-        return SP_LIB["12 o'clock"]                     # Don't need to worry about magnitude
+        return Library.MAIN_LIB["12 o'clock"]                     # Don't need to worry about magnitude
 
     if direction >= 0:                                  # Test if beacon signal is to the left
         if (degree > 15) and (degree <= 45):
-            return SP_LIB["11 o'clock"]
+            return Library.MAIN_LIB["11 o'clock"]
         elif (degree > 45) and (degree <= 75):
-            return SP_LIB["10 o'clock"]
+            return Library.MAIN_LIB["10 o'clock"]
         elif (degree > 75) and (degree <= 90):
-            return SP_LIB["9 o'clock"]
+            return Library.MAIN_LIB["9 o'clock"]
 
     if direction < 0:                                   # Signal to the right
         if (degree > 15) and (degree <= 45):
-            return SP_LIB["1 o'clock"]
+            return Library.MAIN_LIB["1 o'clock"]
         elif (degree > 45) and (degree <= 75):
-            return SP_LIB["2 o'clock"]
+            return Library.MAIN_LIB["2 o'clock"]
         elif (degree > 75) and (degree <= 90):
-            return SP_LIB["3 o'clock"]
+            return Library.MAIN_LIB["3 o'clock"]
 
 # This will display to the user where the heading is
 def dis_head(heading):
-    if heading == SP_LIB["3 o'clock"]:
+    if heading == Library.MAIN_LIB["3 o'clock"]:
         print("Signal Located at 3 o'Clock")
-    elif heading == SP_LIB["2 o'clock"]:
+    elif heading == Library.MAIN_LIB["2 o'clock"]:
         print("Signal Located at 2 o'Clock")
-    elif heading == SP_LIB["1 o'clock"]:
+    elif heading == Library.MAIN_LIB["1 o'clock"]:
         print("Signal Located at 1 o'Clock")
-    elif heading == SP_LIB["12 o'clock"]:
+    elif heading == Library.MAIN_LIB["12 o'clock"]:
         print("Signal Located at 12 o'Clock")
-    elif heading == SP_LIB["11 o'clock"]:
+    elif heading == Library.MAIN_LIB["11 o'clock"]:
         print("Signal Located at 11 o'Clock")
-    elif heading == SP_LIB["10 o'clock"]:
+    elif heading == Library.MAIN_LIB["10 o'clock"]:
         print("Signal Located at 10 o'Clock")
-    elif heading == SP_LIB["9 o'clock"]:
+    elif heading == Library.MAIN_LIB["9 o'clock"]:
         print("Signal Located at 9 o'Clock")
     else:
         print("Error")
